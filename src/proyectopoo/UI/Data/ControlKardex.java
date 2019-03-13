4bcdbd9fbdb4ae0a181c3bb30b01378de2b0e31e
SANTIAGO <SANTIAGO@Santiago>
2019-03-12 20:53:38 -0500

    Merge origin/master
    Conflicts:
    	src/proyectopoo/UI/Data/ProyectoPOO.java

--- src/proyectopoo/UI/Data/ControlKardex.java
+++ src/proyectopoo/UI/Data/ControlKardex.java
@@ -8,12 +8,14 @@ package proyectopoo.UI.Data;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
+import javafx.beans.binding.DoubleBinding;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.StringConverter;
 import proyectopoo.Data.Operaciones;
@@ -54,6 +56,9 @@ public class ControlKardex {
         this.kardex.getTvendidos().setCellValueFactory(new PropertyValueFactory("salidas"));
         this.kardex.getTdetalle().setCellValueFactory(new PropertyValueFactory("detalles"));
         this.kardex.getTvalor().setCellValueFactory(new PropertyValueFactory("valorE"));
+        DoubleBinding usedWidth = this.kardex.getTfecha().widthProperty().add(this.kardex.getTvendidos().widthProperty()).add(this.kardex.getTvalor().widthProperty()).add(this.kardex.getTcomprados().widthProperty()).add(this.kardex.getTexistencias().widthProperty());
+
+        this.kardex.getTdetalle().prefWidthProperty().bind(this.kardex.getTable().widthProperty().subtract(usedWidth));
         this.operaciones=FXCollections.observableArrayList();
         kardex.getTable().setItems(operaciones);
         this.kardex.getSelecproducto().setOnAction(new cambio());
@@ -70,6 +75,7 @@ public class ControlKardex {
             if(kardex.getIni().getValue()!=null&&kardex.getFin().getValue()!=null&&kardex.getSelecproducto().getValue()!=null){
                 ArrayList<Operaciones> o=modelo.getKardex(((Producto)kardex.getSelecproducto().getValue()).getId(), Date.from(kardex.getIni().getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()), Date.from(kardex.getFin().getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
                 operaciones=FXCollections.observableArrayList(o);
+                kardex.getTable().getItems().clear();
                 kardex.getTable().setItems(operaciones);
                 kardex.getTotalExist().setText(String.valueOf(o.get(o.size()-1).getExistencias()));
                 kardex.getValuexist().setText(String.valueOf(o.get(o.size()-1).getValorE()));
@@ -84,7 +90,9 @@ public class ControlKardex {
             if(kardex.getIni().getValue()!=null&&kardex.getFin().getValue()!=null&&kardex.getSelecproducto().getValue()!=null){
                 ArrayList<Operaciones> o=modelo.getKardex(((Producto)kardex.getSelecproducto().getValue()).getId(), Date.from(kardex.getIni().getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()), Date.from(kardex.getFin().getValue().atStartOfDay(ZoneId.systemDefault()).toInstant()));
                 operaciones=FXCollections.observableArrayList(o);
+                kardex.getTable().getItems().clear();
                 kardex.getTable().setItems(operaciones);
+                kardex.getTable().autosize();
                 kardex.getTotalExist().setText(String.valueOf(o.get(o.size()-1).getExistencias()));
                 kardex.getValuexist().setText(String.valueOf(o.get(o.size()-1).getValorE()));
             }
